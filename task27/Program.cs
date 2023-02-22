a06c4261e9aa92189c89c93f24b29d3c2f83dd1c
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-02-22 21:07:26 +0400

    complete task 27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+while (number != 0)
+{
+    int count = number % 10;
+    number = number / 10;
+    summ += count;
+}
+Console.WriteLine($"Сумма цифр равна {summ}");
